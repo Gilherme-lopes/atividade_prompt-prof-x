775d3ef6b00e3ea88b3be98b76dd40416c2a9b2e
Gilherme-lopes <gi.lopessantanacrus@gmail.com>
2024-11-15 16:56:13 -0300

    atividade prompt

---
+++ atividade_promty/js/index.js
@@ -0,0 +1,11 @@
+let cor = prompt("escreva o nome red pra magica acotecer" )
+
+document.querySelector(".body")
+
+if (cor === "red") {
+    document.body.style.backgroundColor = "red";
+} else {
+    alert("errouuuu tente usar a palavra red");
+}
+
+
